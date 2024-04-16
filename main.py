a7a141fd101df437e81752862e0ec3964efb920e
csk200387 <csk200387@gmail.com>
2024-04-17 00:26:34 +0900

    1.1v

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-# if you don't want all intents you can do discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
+
+# check video_nums file and video_queue
+if not os.path.exists("src/video_nums"):
+    with open("src/video_nums", "w") as f:
+        f.write("1")
+
+if not os.path.exists("src/video_queue"):
+    with open("src/video_queue", "w") as f:
+        f.write("")
+
 @client.event
 async def on_ready():
-    # await client.change_presence(status=discord.Status.online, activity=discord.Game(name="/ 로 봇을 사용하세요"))
+    # activity message
     await client.change_presence(status=discord.Status.online, activity=discord.Game(name="AutoShorts Ready"))
     await tree.sync()
     print("Bot is Ready.")
 
 
-@tree.command(name="upload", description="동영상 업로드")
-async def slash_command(interaction: discord.Interaction, url:str):
+@tree.command(name="upload", description="다운로드 & 업로드")
+async def upload_command(interaction: discord.Interaction, url:str):
     await interaction.response.defer()
     msg = await interaction.followup.send("다운로드 중...")
     videoid = download(url)
@@ -29,6 +38,7 @@ async def slash_command(interaction: discord.Interaction, url:str):
 
     await msg.edit(content=f"다운로드 완료. ID : {videoid}\n유튜브로 업로드중...")
 
+    # Recording the number in the title
     with open("src/video_nums", "r") as f:
         partnum = int(f.read())
     with open("src/video_nums", "w") as f:
@@ -39,6 +49,17 @@ async def slash_command(interaction: discord.Interaction, url:str):
 
     await msg.edit(content=f"꺼억~ 업로드 완료. 감사합니다.")
     os.remove(f"{videoid}.webm")
+
+
+@tree.command(name="addqueue", description="대기열에 영상 저장")
+async def queue_command(interaction: discord.Interaction, url:str):
+    await interaction.response.defer()
+    msg = await interaction.followup.send("큐에 추가중입니다..")
+
+    with open("src/video_queue", "a") as f:
+        f.write(url + "\n")
+
+    await msg.edit(content="큐에 추가 완료.")
 
 
 client.run(DISCORD_TOKEN)
